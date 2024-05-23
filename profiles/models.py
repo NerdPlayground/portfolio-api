a1732775a5dc15a6ef020690c9c42246780d7ad1
Nerd <studytime023@gmail.com>
2024-05-24 00:51:58 +0300

    chore (Apps) : configure custom user

--- profiles/models.py
+++ profiles/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+class Profile(models.Model):
+    user=models.OneToOneField(
+        get_user_model(),
+        primary_key=True,
+        on_delete=models.CASCADE,
+    )
+    bio=models.TextField(null=True,blank=True)
+    skills=models.JSONField(null=True,blank=True)
+    socials=models.JSONField(null=True,blank=True)
+
+    def __str__(self):
+        return "%s %s" %(self.user.first_name,self.user.last_name)
