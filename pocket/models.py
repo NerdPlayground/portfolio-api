10bd1e2b0e9d09586612271ae060159c8bcd1bc7
Nerd <studytime023@gmail.com>
2024-05-27 19:15:22 +0300

    chore (Project) : setup base class for projects and experience

--- pocket/models.py
+++ pocket/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Achievement(models.Model):
+    link=models.URLField(null=True,blank=True)
+    start_date=models.DateField(null=True,blank=True)
+    end_date=models.DateField(null=True,blank=True)
+    ongoing=models.BooleanField(default=False,blank=True)
+    description=models.TextField(null=True,blank=True)
+    objectives=models.JSONField(null=True,blank=True)
+    tools=models.JSONField(null=True,blank=True)
+
+    class Meta:
+        abstract=True
