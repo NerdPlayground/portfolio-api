d477e70fddd2df70f804a67b4a9da80849e892be
Nerd <studytime023@gmail.com>
2024-06-04 17:03:13 +0300

    test (Apps) : test experiences endpoints

--- pocket/tests.py
+++ pocket/tests.py
@@ -2,15 +2,20 @@ from django.urls import reverse
 from django.test import TestCase
 from profiles.factories import ProfilesFactory
 from projects.factories import ProjectsFactory
+from experiences.factories import ExperiencesFactory
 
 class PocketTestCase(TestCase):
     @classmethod
     def setUpTestData(cls):
         cls.password="qwerty123!@#"
+        
         cls.admin=ProfilesFactory.create(user__is_staff=True)
         cls.admin_projects=ProjectsFactory.create_batch(2,user=cls.admin.user)
+        cls.admin_experiences=ExperiencesFactory.create_batch(2,user=cls.admin.user)
+
         cls.member,cls.other_member=ProfilesFactory.create_batch(2)
         cls.member_projects=ProjectsFactory.create_batch(2,user=cls.member.user)
+        cls.member_experiences=ExperiencesFactory.create_batch(2,user=cls.member.user)
 
     def member_login(self,member,password=None):
         password=password or self.password
