cb79616932cb081a68e704c84b8651aea3dca8cc
Nerd <studytime023@gmail.com>
2024-06-11 14:37:03 +0300

    refractor (Tests) : use knox endpoints and token authentication

--- profiles/tests.py
+++ profiles/tests.py
@@ -2,8 +2,10 @@ import re
 from .models import Profile
 from django.core import mail
 from django.urls import reverse
+from knox.models import AuthToken
 from pocket.tests import PocketTestCase
-from rest_framework.authtoken.models import Token
+from django.contrib.auth import get_user_model
+from allauth.account.models import EmailAddress
 
 '''
 TESTED ENDPOINTS:
@@ -11,9 +13,7 @@ TESTED ENDPOINTS:
 - register user
 - verify user email
 - change user details
-- change user details (intruder)
 - delete user
-- delete user (intruder)
 - change password
 - reset password
 - admin access all users
@@ -27,15 +27,20 @@ class ProfilesTestCase(PocketTestCase):
     def setUpTestData(cls):
         super().setUpTestData()
     
-    def verify_user_email(self):
-        pattern='http://testserver/api/v1/dj-rest-auth/registration/account-confirm-email/(?P<key>[-:\w]+)/'
+    def verify_user_email(self,username):
+        pattern="token: (?P<key>[-:\w]+)"
         key=re.search(pattern,mail.outbox[0].body).group("key")
         response=self.client.post(reverse("rest_verify_email"),{"key":key})
         self.assertEqual(response.status_code,200)
 
-    def setup_profile_details(self,username,email,details):
+        current_user=get_user_model().objects.get(username=username)
+        email_address=EmailAddress.objects.get(user=current_user)
+        self.assertTrue(email_address.verified)
+
+    def setup_profile_details(self,token,username,email,details):
         response=self.client.put(
-            path=reverse("user-detail",kwargs={"username":username}),
+            headers={"Authorization": f"Bearer {token}"},
+            path=reverse("current-user"),
             content_type="application/json",
             data={
                 'username':username,'email':email,
@@ -48,7 +53,7 @@ class ProfilesTestCase(PocketTestCase):
 
     def test_member_registration(self):
         username="dorobu"
-        email="{}@gmail.com".format(username)
+        email=f"{username}@gmail.com"
         response=self.client.post(reverse("rest_register"),{
             "username":username,
             "email":email,
@@ -57,8 +62,10 @@ class ProfilesTestCase(PocketTestCase):
         })
         self.assertEqual(response.status_code,204)
         self.assertEqual(len(mail.outbox),1)
-        self.verify_user_email()
+        self.verify_user_email(username)
 
+        profile=Profile.objects.last()
+        token=self.member_login(profile,self.password)
         details={
             "first_name":"George",
             "last_name":"Mobisa",
@@ -72,7 +79,7 @@ class ProfilesTestCase(PocketTestCase):
                 }
             }
         }
-        self.setup_profile_details(username,email,details)
+        self.setup_profile_details(token,username,email,details)
 
         profile=Profile.objects.last()
         self.assertEqual(profile.user.username,username)
@@ -83,54 +90,44 @@ class ProfilesTestCase(PocketTestCase):
         self.assertEqual(profile.skills,details.get('profile').get('skills'))
         self.assertEqual(profile.socials,details.get('profile').get('socials'))
 
-
-    def test_intruder_change_information(self):
-        self.member_login(self.other_member)
-        response=self.client.put(reverse(
-            "user-detail",
-            kwargs={"username":self.member.user.username}
-        ))
-        self.assertEqual(response.status_code,403)
-
     def test_member_delete_account(self):
-        self.member_login(self.member)
+        token=self.member_login(self.member)
         total_members=Profile.objects.count()
-        response=self.client.delete(reverse(
-            "user-detail",
-            kwargs={"username":self.member.user.username}
-        ))
+        response=self.client.delete(
+            path=reverse("current-user"),
+            headers={"Authorization": f"Bearer {token}"},
+        )
         self.assertEqual(response.status_code,204)
         self.assertEqual(Profile.objects.count(),total_members-1)
 
-    def test_intruder_delete_account(self):
-        self.member_login(self.other_member)
-        response=self.client.delete(reverse(
-            "user-detail",
-            kwargs={"username":self.member.user.username}
-        ))
-        self.assertEqual(response.status_code,403)
-
     def test_member_change_password(self):
-        self.member_login(self.member)
+        token=self.member_login(self.member)
         password="QWERTY23!@#"
-        response=self.client.post(reverse("rest_password_change"),{
-            "old_password":self.password,
-            "new_password1":password,
-            "new_password2":password,
-        })
+        response=self.client.post(
+            headers={"Authorization": f"Bearer {token}"},
+            path=reverse("rest_password_change"),
+            data={
+                "old_password":self.password,
+                "new_password1":password,
+                "new_password2":password,
+            }
+        )
         self.assertEqual(response.status_code,200)
 
-        user_response=self.get_current_user()
-        self.assertEqual(user_response.status_code,403)
+        logout_response=self.client.post(
+            path=reverse("knox_logout"),
+            headers={"Authorization":f"Bearer {token}"},
+        )
+        self.assertEqual(logout_response.status_code,204)
         
-        login_response=self.member_login(self.member,password)
-        self.assertIn("key",login_response)
+        new_token=self.member_login(self.member,password)
+        user_response=self.get_current_user(new_token)
+        self.assertEqual(user_response.status_code,200)
 
     def test_member_password_reset(self):
-        self.member_login(self.member)
         response=self.client.post(
-            reverse('password_reset:reset-password-request'),
-            {"email":self.member.user.email}
+            path=reverse("password_reset:reset-password-request"),
+            data={"email":self.member.user.email},
         )
         self.assertEqual(response.status_code,200)
         self.assertEqual(len(mail.outbox),1)
@@ -138,47 +135,54 @@ class ProfilesTestCase(PocketTestCase):
         pattern="Authentication Token: (?P<token>[\w]+)"
         token=re.search(pattern,mail.outbox[0].body).group("token")
         token_response=self.client.post(
-            reverse("password_reset:reset-password-validate"),
-            {"token":token}
+            path=reverse("password_reset:reset-password-validate"),
+            data={"token":token}
         )
         self.assertEqual(token_response.status_code,200)
 
         password="QWERTY23!@#"
         reset_response=self.client.post(
-            reverse('password_reset:reset-password-confirm'),
-            {"token":token,"password":password,}
+            path=reverse("password_reset:reset-password-confirm"),
+            data={"token":token,"password":password,}
         )
         self.assertEqual(reset_response.status_code,200)
-
-        user_response=self.get_current_user()
-        self.assertEqual(user_response.status_code,403)
         
-        login_response=self.member_login(self.member,password)
-        self.assertIn("key",login_response)
+        new_token=self.member_login(self.member,password)
+        user_response=self.get_current_user(new_token)
+        self.assertEqual(user_response.status_code,200)
     
     def test_member_logout(self):
-        token_counter=Token.objects.count()
-        self.member_login(self.member)
-        self.assertEqual(Token.objects.count(),token_counter+1)
+        token_counter=AuthToken.objects.count()
+        token=self.member_login(self.member)
+        self.assertEqual(AuthToken.objects.count(),token_counter+1)
 
-        user_response=self.get_current_user()
+        user_response=self.get_current_user(token)
         self.assertEqual(user_response.status_code,200)
 
-        response=self.client.post(reverse("rest_logout"))
-        self.assertEqual(response.status_code,200)
-        self.assertEqual(Token.objects.count(),token_counter)
+        response=self.client.post(
+            path=reverse("knox_logout"),
+            headers={"Authorization":f"Bearer {token}"},
+        )
+        self.assertEqual(response.status_code,204)
+        self.assertEqual(AuthToken.objects.count(),token_counter)
 
-        user_response=self.get_current_user()
-        self.assertEqual(user_response.status_code,403)
+        user_response=self.get_current_user(token)
+        self.assertEqual(user_response.status_code,401)
     
     def test_admin_access_all_users(self):
-        self.member_login(self.admin)
+        token=self.member_login(self.admin)
         profile_count=Profile.objects.count()
-        response=self.client.get(reverse("user-list"))
+        response=self.client.get(
+            path=reverse("user-list"),
+            headers={"Authorization":f"Bearer {token}"},
+        )
         self.assertEqual(response.status_code,200)
         self.assertEqual(len(response.json()),profile_count)
     
     def test_member_access_all_users(self):
-        self.member_login(self.member)
-        response=self.client.get(reverse("user-list"))
+        token=self.member_login(self.member)
+        response=self.client.get(
+            path=reverse("user-list"),
+            headers={"Authorization":f"Bearer {token}"},
+        )
         self.assertEqual(response.status_code,403)
